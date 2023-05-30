715e6b72cbb5452437493f63bf592b4ecaf3beea
Artem Kobzar <artem.kobzar@jetbrains.com>
2023-05-30 15:14:10 +0200

    Fixate the size of wasm binary after objects optimization

---
+++ compiler/testData/codegen/box/size/objectsOptimization.kt
@@ -0,0 +1,43 @@
+// TARGET_BACKEND: WASM
+
+// WASM_DCE_EXPECTED_OUTPUT_SIZE: wasm 15_878
+// WASM_DCE_EXPECTED_OUTPUT_SIZE:  mjs 5_450
+
+object Simple
+
+object SimpleWithPureProperty {
+    val text = "Hello"
+}
+
+object SimpleWithPropertyInitializedDurintInit {
+    val text: String
+    init {
+        text = "Hello"
+    }
+}
+
+object SimpleWithFunctionsOnly {
+    fun foo() = "Foo"
+    fun bar() = "Bar"
+}
+
+interface Callable {
+    fun call(): String
+}
+
+object SimpleWithInterface : Callable {
+    override fun call() = "OK"
+}
+
+object UsedGetFieldInside {
+    val anotherText = SimpleWithPureProperty.text
+}
+
+fun box(): String {
+    if (SimpleWithPureProperty.text != "Hello") return "Fail simple case with pure property"
+    if (SimpleWithPropertyInitializedDurintInit.text != "Hello") return "Fail simple case with pure property initialized inside init block"
+    if (SimpleWithFunctionsOnly.foo() != "Foo" || SimpleWithFunctionsOnly.bar() != "Bar") return "Fail simple case with functions only"
+    if (SimpleWithInterface.call() != "OK") return "Fail simple case with interface implementing"
+    if (UsedGetFieldInside.anotherText != "Hello") return "Fail object which used another object inside its initialization block"
+    return "OK"
+}
