dd9ad5bd4637c9e5ae6b6352f7200a779f588fa1
liuwei11 <liuwei11@kuaishou.com>
2023-06-13 23:45:19 +0800

    新增时间

--- compiler/psi/src/org/jetbrains/kotlin/parsing/KotlinParser.java
+++ compiler/psi/src/org/jetbrains/kotlin/parsing/KotlinParser.java
@@ -42,6 +42,7 @@ public class KotlinParser implements PsiParser {
     // we need this method because we need psiFile
     @NotNull
     public ASTNode parse(IElementType iElementType, PsiBuilder psiBuilder, PsiFile psiFile) {
+        long startTime = System.currentTimeMillis();
         KotlinParsing ktParsing = KotlinParsing.createForTopLevel(new SemanticWhitespaceAwarePsiBuilderImpl(psiBuilder));
         String extension = FileUtilRt.getExtension(psiFile.getName());
         if (extension.isEmpty() || extension.equals(KotlinFileType.EXTENSION) || (psiFile instanceof KtFile && ((KtFile) psiFile).isCompiled())) {
@@ -50,6 +51,7 @@ public class KotlinParser implements PsiParser {
         else {
             ktParsing.parseScript();
         }
+        System.out.println("[TimeTest] " + psiFile.getName() + "parse cost " + (System.currentTimeMillis() - startTime));
         return psiBuilder.getTreeBuilt();
     }
 
